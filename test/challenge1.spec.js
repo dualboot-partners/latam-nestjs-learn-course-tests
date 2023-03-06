4c4fad5073a4c294a15157c8a2b2641569df7f5d
Javier Meza <javiermeza@192.168.1.17>
2023-03-06 11:58:59 -0500

    updating test based on nestjs course feedback

--- test/challenge1.spec.js
+++ test/challenge1.spec.js
@@ -16,43 +16,33 @@ describe("Testing challenge #1", () => {
       });
   });
 
-  it("findAll - should successfully retrieve the chain {findAll function getting all dogs}", (done) => {
-    request(baseUrl)
-      .get("/dog")
-      .set("Accept", "application/json")
-      .set("Content-Type", "application/json")
-      .end(function (err, res) {
-        expect(res.statusCode).to.be.equal(200);
-        expect(res.text).to.be.string("findAll function getting all dogs");
-        done();
-      });
-  });
+  
 
-  it("find - should successfully retrieve the chain {find function with params age: 8 & race: Pitbull}", (done) => {
+  it("find - should successfully retrieve the chain {find function with params age: 8 & breed: Pitbull}", (done) => {
     request(baseUrl)
       .get("/dog")
-      .query({ race: "Pitbull", age: 8 })
+      .query({ breed: "Pitbull", age: 8 })
       .set("Accept", "application/json")
       .set("Content-Type", "application/json")
       .end(function (err, res) {
         expect(res.statusCode).to.be.equal(200);
         expect(res.text).to.be.string(
-          "find function with params age: 8 & race: Pitbull"
+          "find function with params breed: Pitbull & age: 8"
         );
         done();
       });
   });
 
-  it("create - should successfully retrieve the chain {create function with properties, race: Poddle, age: 12 & color: white}", (done) => {
+  it("create - should successfully retrieve the chain {create function with properties, breed: Poddle, age: 12 & color: white}", (done) => {
     request(baseUrl)
       .post("/dog")
-      .send({ race: "Poddle", age: 12, color: "white" })
+      .send({ breed: "Poddle", age: 12, color: "white" })
       .set("Accept", "application/json")
       .set("Content-Type", "application/json")
       .end(function (err, res) {
         expect(res.statusCode).to.be.equal(201);
         expect(res.text).to.be.string(
-          "create function with properties, race: Poddle, age: 12 & color: white"
+          "create function with properties, breed: Poddle, age: 12 & color: white"
         );
         done();
       });
